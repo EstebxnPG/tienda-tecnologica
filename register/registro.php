208c0b1f1ef3779ece5c1ab0e6caed6423b8aac8
Esteban Patiño <gaonaestebandpg@gmail.com>
2025-04-02 18:08:59 -0500

    Includes login y php
    Includes login y register, php
    
    Co-Authored-By: Jacob Burkhart <10033+jacobo@users.noreply.github.com>

--- register/registro.php
+++ register/registro.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>THETECMENS</title>
     <link rel="stylesheet" href="../Login/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    
 </head>
 <body>
+    
+    <!-- Nav bar -->
+    <?php include '../assets/includes/header.php'; ?>
+
     <div class="super__container">
         <div class="container">
-            
-            <div class="login__container">
+            <di v class="login__container">
                 <img src="/STYLES_index_login/img/logo-sena.png" alt="" class="logo__container">
                 <h2 class="title__container"> INICIA SESIÓN </h2>
                 <p class="welcome__container"> Bienvenido a la tienda THETECMENS </p>
                 <form class="form login__form">
+                    <label class="form__label" for="email">Nombre completo</label>
+                    <input type="email" class="form__input" id="email" name="email" placeholder="Ingresa tu correo" >
+                    <br>
+                    <label class="form__label" for="password">telefono</label>
+                    <input type="tel" class="form__input" id="tel" name="tel" placeholder="Ingresa tu contraseña">
+                    <br>
                     <label class="form__label" for="email">Correo Electrónico</label>
                     <input type="email" class="form__input" id="email" name="email" placeholder="Ingresa tu correo" >
+                    <br>
+                    <label class="form__label" for="password">Confirmacion de correo eletronico</label>
+                    <input type="email" class="form__input" id="email" name="email" placeholder="Ingresa tu contraseña">
                     <br>
                     <label class="form__label" for="password">Contraseña</label>
                     <input type="password" class="form__input" id="password" name="password" placeholder="Ingresa tu contraseña">
@@ -25,9 +40,9 @@
                 
                     <button type="submit" class="login-button__form">INICIAR SESIÓN</button>
                 </form>
-            </div>
+            </di>
 
         </div>
-    </div>
+    </div>  
 </body>
 </html>
