1e685e58ecf04e2b881adf74ea3fa1074a3e6f91
Esteban Patiño <gaonaestebandpg@gmail.com>
2025-04-02 17:43:33 -0500

    Index
    Re estructuración de el index en los includes
    
    Co-Authored-By: Jacob Burkhart <10033+jacobo@users.noreply.github.com>

---
+++ assets/includes/slider.php
@@ -0,0 +1,7 @@
+<div class="slider">
+  <ul>
+    <li><img src="assets\img\slide1.avif" alt="..."></li>
+    <li><img src="assets\img\slide2.jpg" alt="..."></li>
+    <li><img src="assets\img\slide3.png" alt="..."></li>
+  </ul>
+</div>
