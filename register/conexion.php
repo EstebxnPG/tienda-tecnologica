42c38d64b905f1113e91ac1b73c0ebc480195159
stephanny <stephannycruzzuluaga@gmail.com>
2025-04-03 21:59:21 -0500

    se borro unos comentarios

--- register/conexion.php
+++ register/conexion.php
@@ -4,10 +4,9 @@ $username = "root";
 $password = "123456"; 
 $database = "thetecmens"; 
 
-// Crear la conexión
+
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Error en la conexión a la base de datos: " . $conn->connect_error);
 }
