17ba5d8a99aff50aecb436af5ee90f69bd1530bf
Esteban Patiño <gaonaestebandpg@gmail.com>
2025-04-04 00:01:36 -0500

    Acomodacion tienda_sena
    En la conexiòn en php, debe ser el nombre de la base de datos, tienda_sena.

--- register/conexion.php
+++ register/conexion.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost"; 
 $username = "root"; 
-$password = "123456"; 
-$database = "thetecmens"; 
+$password = ""; 
+$database = "tienda_sena"; 
 
 
 $conn = new mysqli($servername, $username, $password, $database);
